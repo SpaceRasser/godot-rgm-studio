c06513dd67ac60eeaac5a6c714a8ddc8494d8aca
grass500 <grasss500@gmail.com>
2023-06-27 19:53:57 +0400

    grass
    Добавил начальное меню, заглушку под play и about.

---
+++ Scenes/PlayButton.gd
@@ -0,0 +1,8 @@
+extends Control
+
+func _on_pressed():
+	get_tree().change_scene_to_file("res://Scenes/world.tscn")
+
+
+func _on_about_button_pressed():
+	pass # Replace with function body.
---
+++ Scenes/main_menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/world.tscn")
+
+func _on_about_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/about.tscn")
+
+func _on_quit_button_pressed():
+	get_tree().quit()
+
+
+
+
