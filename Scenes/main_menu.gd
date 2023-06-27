7ac1e0309ad0ea74bc857036cb9a8ca86d9c6d43
grass500 <grasss500@gmail.com>
2023-06-27 23:23:23 +0400

    grass7
    ничего

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -4,7 +4,7 @@ func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/world.tscn")
 
 func _on_about_button_pressed():
-	get_tree().change_scene_to_file("res://Scenes/about.tscn")
+	get_tree().change_scene_to_file("res://Scenes/test_world.tscn")
 
 func _on_quit_button_pressed():
 	get_tree().quit()
--- Scenes/mobs/mushroom_mob.gd
+++ Scenes/mobs/mushroom_mob.gd
@@ -2,28 +2,33 @@ extends CharacterBody2D
 
 var SPEED = 50
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var player 
+var player
 var chase = false
 
+func _ready():
+	get_node("AnimatedSprite2D").play("Idle")
+	
 func _physics_process(delta):
+	#Gravity for Frog
 	velocity.y += gravity * delta
 	if chase == true:
 		player = get_node("../../player/player")
 		var direction = (player.position - self.position).normalized()
-		if direction.x > 0 :
+		if direction.x > 0:
 			get_node("AnimatedSprite2D").flip_h = true
 		else:
 			get_node("AnimatedSprite2D").flip_h = false
 		velocity.x = direction.x * SPEED
 	else:
-		get_node("AnimatedSprite2D").play("idle")
+		get_node("AnimatedSprite2D").play("Idle")
 		velocity.x = 0
 	move_and_slide()
-
+	
 func _on_player_detection_body_entered(body):
 	if body.name == "player":
 		chase = true
-	
+
+
 func _on_player_detection_body_exited(body):
 	if body.name == "player":
 		chase = false
